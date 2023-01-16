615f4ca4ee0fa77276bba5a245ca73645c09c07e
muham <muhammadaaa727@gmail.com>
2023-01-16 17:05:55 +0300

    todo app

--- lib/screens/temp_screen.dart
+++ lib/screens/temp_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TempScreen extends StatelessWidget {
+
+class TempScreen extends StatefulWidget {
   const TempScreen({Key? key}) : super(key: key);
 
+  @override
+  _TempScreenState createState() => _TempScreenState();
+}
+
+class _TempScreenState extends State<TempScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
